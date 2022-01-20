7c4b60b2912ec7e288c491a2bf810c6b299f2e09
Oleh Potomakha <potomakha@gmail.com>
2022-01-20 18:49:37 +0200

    employes / config Office Title

---
+++ Module4HW3/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Module4HW3
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Employee> Companies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new Compa);
+        }
+    }
+}
---
+++ Module4HW3/EntityConfig/EmployeeConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Entity;
+
+namespace Module4HW3.EntityConfig
+{
+    public class EmployeeConfig : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.ToTable("Employee").HasKey(e => e.EmployeId);
+            builder.Property(e => e.EmployeId).HasColumnName("ID").ValueGeneratedOnAdd();
+            builder.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.LastName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.DateOfBirth).HasColumnType("Date");
+            builder.Property(e => e.OfficeId);
+            builder.HasOne(e => e.Office)
+                .WithMany(o => o.Employees)
+                .HasForeignKey(e => e.EmployeId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
