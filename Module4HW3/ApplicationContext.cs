b9694818c92b8e00347c01c9837164c23660bdc0
Oleh Potomakha <potomakha@gmail.com>
2022-01-21 12:40:09 +0200

    db connection

--- Module4HW3/ApplicationContext.cs
+++ Module4HW3/ApplicationContext.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Module4HW3.Entity;
+using Module4HW3.EntityConfig;
 
 namespace Module4HW3
 {
     public class ApplicationContext : DbContext
     {
-        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
         {
             Database.EnsureCreated();
         }
 
         public DbSet<Employee> Companies { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<EmployeeProject> EmployeeProjects { get; set; }
+        public DbSet<Project> Projects { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new Compa);
+            modelBuilder.ApplyConfiguration(new EmployeeConfig());
+            modelBuilder.ApplyConfiguration(new TitleConfig());
+            modelBuilder.ApplyConfiguration(new OfficeConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeProjectConfig());
+            modelBuilder.ApplyConfiguration(new ProjectConfig());
         }
     }
 }
--- Module4HW3/Program.cs
+++ Module4HW3/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace Module4HW3
 {
@@ -6,7 +9,16 @@ namespace Module4HW3
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var optionDbBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            var options = optionDbBuilder
+                .UseSqlite(connectionString)
+                .Options;
 
+            using (var db = new ApplicationContext(options))
         }
     }
 }
