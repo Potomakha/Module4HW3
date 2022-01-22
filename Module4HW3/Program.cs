f1914088eb84ae352a81a581ce5e6e4800b119fb
Oleh Potomakha <potomakha@gmail.com>
2022-01-22 20:34:23 +0200

    EF settings Initial migration

--- Module4HW3/Program.cs
+++ Module4HW3/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
+﻿using System;
 
 namespace Module4HW3
 {
@@ -9,18 +6,7 @@ namespace Module4HW3
     {
         public static void Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json").Build();
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            var optionDbBuilder = new DbContextOptionsBuilder<ApplicationContext>();
-            var options = optionDbBuilder
-                .UseSqlServer(connectionString)
-                .Options;
-
-            using (var db = new ApplicationContext(options))
-            {
-            }
+            Console.Write("Started");
         }
     }
 }
---
+++ Module4HW3/SampleContextFatory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Module4HW3
+{
+    public class SampleContextFatory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var optionDbBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            var options = optionDbBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            return new ApplicationContext(options);
+        }
+    }
+}
