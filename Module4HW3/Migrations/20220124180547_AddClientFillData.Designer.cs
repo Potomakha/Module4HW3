17c5295db6cf66ad24aa99028f96a20de2164b0f
Oleh Potomakha <potomakha@gmail.com>
2022-01-24 20:06:50 +0200

    new migration

--- Module4HW3/Migrations/20220124180547_AddClientFillData.Designer.cs
+++ Module4HW3/Migrations/20220124180547_AddClientFillData.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Module4HW3;
 
 namespace Module4HW3.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220124180547_AddClientFillData")]
+    partial class AddClientFillData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Module4HW3/Migrations/20220124180547_AddClientFillData.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Module4HW3.Migrations
+{
+    public partial class AddClientFillData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrganizationName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CooperationStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CooperationEnd = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.ClientId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Client",
+                columns: new[] { "ClientId", "CooperationEnd", "CooperationStart", "FirstName", "LastName", "OrganizationName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dima", "Dimich", "Kulinichi" },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Vanya", "Vano", "Bufet" },
+                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Oleh", "Olegich", "ATB market" },
+                    { 4, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Alyona", "Alyeona", "Microsoft" },
+                    { 5, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Petro", "Petrovich", "Apple" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Project",
+                columns: new[] { "ProjectId", "Budget", "ClientId", "Name", "StaredTime" },
+                values: new object[,]
+                {
+                    { 1, 1000m, 1, "calculator", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 2123m, 2, "Windows 10", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, 1124512m, 3, "onlineShop", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, 100123m, 4, "music player", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, 1246100m, 5, "vr home", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ClientId",
+                table: "Project",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Client_ClientId",
+                table: "Project",
+                column: "ClientId",
+                principalTable: "Client",
+                principalColumn: "ClientId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Client_ClientId",
+                table: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Project_ClientId",
+                table: "Project");
+
+            migrationBuilder.DeleteData(
+                table: "Project",
+                keyColumn: "ProjectId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Project",
+                keyColumn: "ProjectId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Project",
+                keyColumn: "ProjectId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Project",
+                keyColumn: "ProjectId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Project",
+                keyColumn: "ProjectId",
+                keyValue: 5);
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Project");
+        }
+    }
+}
